2abc9256ee1ed7fc18fee79198ea7c3a4bea1178
ziqi-02 <ahmadarroziqi@gmail.com>
2024-06-29 03:06:03 +0700

    setup model profile

---
+++ lib/models/pages/profile/data_pengemudi/data_pengemudi_items.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_app/models/pages/profile/data_pengemudi/data_pengemudi_class.dart';
+
+class DataPengemudiItems {
+  List<DataPengemudiClass> items = [
+    // lanjutkan dengan mengisi sesuai kolom input yang ada di halaman data pengemudi
+    DataPengemudiClass(label: 'Nama', type: 'text', name: 'nama'),
+  ];
+}
---
+++ lib/models/pages/profile/profile_items.dart
@@ -0,0 +1,8 @@
+import 'profile_class.dart';
+
+class ProfileItems {
+  List<ProfileClass> items = [
+    // isi sesuai fitur yang ada di profile page
+    ProfileClass(imageUrl: 'assets/img/icon/profil.png', title: 'Data Pengemudi', routeName: '/profile/data-pengemudi'),
+  ];
+}
