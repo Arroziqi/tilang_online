bb3971a082a02131717ca721cb1c7a590d616dc7
ziqi-02 <ahmadarroziqi@gmail.com>
2024-06-30 02:10:35 +0700

    set up model untuk halaman edukasi

---
+++ lib/models/pages/edukasi/tutorial/tutorial_items.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_app/models/pages/edukasi/tutorial/tutorial_class.dart';
+
+class TutorialItems{
+  List<TutorialClass> items = [
+    // todo: lengkapi sesuai dengan yang di desain figma
+    TutorialClass(title: 'title', imgUrl: 'assets/img/', bodyText: 'bodyText')
+  ];
+}
